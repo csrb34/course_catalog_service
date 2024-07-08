e4c5f127a662591ee066ad6f6e949d836bdcae65
Carla San Roman <carla.s@thoughtworks.com>
2024-07-08 16:37:01 +0200

    build PUT endpoint to update a course

---
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/exception/CourseNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.kotlinspring.course_catalog_service.exception
+
+//import java.lang.RuntimeException
+
+class CourseNotFoundException(message: String) : RuntimeException(message)
--- src/main/kotlin/com/kotlinspring/course_catalog_service/service/CourseService.kt
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/service/CourseService.kt
@@ -2,8 +2,10 @@ package com.kotlinspring.course_catalog_service.service
 
 import com.kotlinspring.course_catalog_service.dto.CourseDTO
 import com.kotlinspring.course_catalog_service.entity.Course
+import com.kotlinspring.course_catalog_service.exception.CourseNotFoundException
 import com.kotlinspring.course_catalog_service.repository.CourseRepository
 import mu.KLogging
+import org.hibernate.annotations.NotFound
 import org.springframework.stereotype.Service
 
 @Service
@@ -31,5 +33,21 @@ class CourseService(val courseRepository: CourseRepository) {
             .map {
                 CourseDTO(it.id, it.name, it.category)
             }
+    }
+
+    fun updateCourse(courseId: Int, courseDTO: CourseDTO): CourseDTO {
+        val existingCourse = courseRepository.findById(courseId)
+
+        return if (existingCourse.isPresent){
+            existingCourse.get()
+                .let {
+                    it.name = courseDTO.name
+                    it.category = courseDTO.category
+                    courseRepository.save(it)
+                    CourseDTO(it.id, it.name, it.category)
+                }
+        }else{
+            throw CourseNotFoundException("No course found with ID: $courseId")
+        }
     }
 }
