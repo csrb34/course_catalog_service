3f2ff6e78d6d02cde35072722ab391e06904db8b
Carla San Roman <carla.s@thoughtworks.com>
2024-07-09 17:19:22 +0200

    retrieve courses by name using JPA query

--- src/main/kotlin/com/kotlinspring/course_catalog_service/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/repository/CourseRepository.kt
@@ -3,4 +3,10 @@ package com.kotlinspring.course_catalog_service.repository
 import com.kotlinspring.course_catalog_service.entity.Course
 import org.springframework.data.repository.CrudRepository
 
-interface CourseRepository : CrudRepository<Course, Int>
+interface CourseRepository : CrudRepository<Course, Int>{
+
+    // JPA Query methods docs
+    // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.query-methods.query-creation
+
+    fun findByNameContaining(courseName: String): List<Course>
+}
