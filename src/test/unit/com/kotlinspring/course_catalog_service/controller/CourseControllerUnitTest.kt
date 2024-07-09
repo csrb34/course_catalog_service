1ad74c256626860493fd94fcbb8bc024ea7207c5
Carla San Roman <carla.s@thoughtworks.com>
2024-07-09 15:16:47 +0200

    setting up unit tests for CourseController

---
+++ src/test/unit/com/kotlinspring/course_catalog_service/controller/CourseControllerUnitTest.kt
@@ -0,0 +1,19 @@
+package com.kotlinspring.course_catalog_service.controller
+
+import com.kotlinspring.course_catalog_service.service.CourseService
+import com.ninjasquad.springmockk.MockkBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebMvcTest(controllers = [CourseController::class])
+@AutoConfigureWebTestClient
+class CourseControllerUnitTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var courseServiceMockk: CourseService
+}
