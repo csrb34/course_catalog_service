8726c1cd590ad31fc379a7655102c1ab9726ab5b
Carla San Roman <carla.s@thoughtworks.com>
2024-07-10 15:18:42 +0200

    retrieve courses by name using native SQL query

--- src/main/kotlin/com/kotlinspring/course_catalog_service/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/repository/CourseRepository.kt
@@ -1,12 +1,16 @@
 package com.kotlinspring.course_catalog_service.repository
 
 import com.kotlinspring.course_catalog_service.entity.Course
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository : CrudRepository<Course, Int>{
 
     // JPA Query methods docs
     // https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html#jpa.query-methods.query-creation
-
     fun findByNameContaining(courseName: String): List<Course>
+
+    // Native SQL query
+    @Query(value="SELECT * FROM COURSES where name like %?1%", nativeQuery = true)
+    fun findCoursesByName(courseName: String): List<Course>
 }
