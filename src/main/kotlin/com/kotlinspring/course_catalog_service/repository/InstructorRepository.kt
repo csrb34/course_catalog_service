bdfd76e7af4663b01a2eb64fd4c9d80a811df4e2
Carla San Roman <carla.s@thoughtworks.com>
2024-07-11 15:27:13 +0200

    add POST endpoint to create an Instructor

---
+++ src/main/kotlin/com/kotlinspring/course_catalog_service/repository/InstructorRepository.kt
@@ -0,0 +1,6 @@
+package com.kotlinspring.course_catalog_service.repository
+
+import com.kotlinspring.course_catalog_service.entity.Instructor
+import org.springframework.data.repository.CrudRepository
+
+interface InstructorRepository: CrudRepository<Instructor, Int>
